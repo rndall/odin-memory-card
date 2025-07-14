741fee3206131c4853663e184ae89e9593e4edd7
rndall <96964503+rndall@users.noreply.github.com>
2025-07-14 16:36:54 +0800

    refactor: move Header to components dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import Header from "./Header"
+import Header from "./components/Header"
 
 const queryClient = new QueryClient()
 
