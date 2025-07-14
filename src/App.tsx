174af5947c6e36158e767e612c64129dcacc9fd0
rndall <96964503+rndall@users.noreply.github.com>
2025-07-14 16:17:25 +0800

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import Header from "./Header"
 
 const queryClient = new QueryClient()
 
 function App() {
-  return <QueryClientProvider client={queryClient}>App</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Header />
+    </QueryClientProvider>
+  )
 }
 
 export default App
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        Memory Card Game
+      </h1>
+    </header>
+  )
+}
+
+export default Header
