2c0ff99ada0930ab26c5a0803b869e2dd1087701
rndall <96964503+rndall@users.noreply.github.com>
2025-07-15 13:34:30 +0800

    feat: add padding block to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 function Container({ children }: ContainerProps) {
   return (
-    <div className="mx-auto px-4 sm:max-w-[40rem] md:max-w-[48rem] lg:max-w-[64rem] lg:px-8 xl:max-w-[80rem] 2xl:max-w-[96rem]">
+    <div className="mx-auto px-4 py-4 sm:max-w-[40rem] md:max-w-[48rem] lg:max-w-[64rem] lg:px-8 xl:max-w-[80rem] 2xl:max-w-[96rem]">
       {children}
     </div>
   )
