441516326cfecdd13ef2a0e7b3328acd68c62520
rndall <96964503+rndall@users.noreply.github.com>
2025-07-15 13:49:02 +0800

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "./components/theme-provider"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Header from "./components/Header"
 import Main from "./components/Main"
@@ -6,10 +7,12 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Header />
-      <Main />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Main />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
