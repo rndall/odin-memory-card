ee4e182fdfab64a1763cba59fd1293791a0269fc
rndall <96964503+rndall@users.noreply.github.com>
2025-07-15 14:10:17 +0800

    feat: add directions in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,10 @@ import Container from "./Container"
 
 function Header() {
   return (
-    <header>
+    <header className="text-center">
       <Container>
         <h1>Memory Card Game</h1>
+        <p>Don't click the same champion twice!</p>
       </Container>
     </header>
   )
