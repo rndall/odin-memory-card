8015c9b8b1ecd63a9bd8ccdcfd8576fe80ad2fb0
rndall <96964503+rndall@users.noreply.github.com>
2025-07-15 12:12:46 +0800

    refactor: h1 styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-        Memory Card Game
-      </h1>
+      <h1>Memory Card Game</h1>
     </header>
   )
 }
