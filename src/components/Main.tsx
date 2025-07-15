16d8b30122d20008f78152b8c1ac73ea48b09b3f
rndall <96964503+rndall@users.noreply.github.com>
2025-07-15 16:59:43 +0800

    feat: randomize champion names

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,35 @@
+import { useChampionNamesQuery } from "@/features/champions/hooks/useChampionNamesQuery"
+import uniqueIndexes from "@/utils/uniqueIndexes"
+import { useState, useEffect } from "react"
 import Container from "./Container"
+import Scoreboard from "@/features/scoreboard/components/Scoreboard"
+import ChampionCard from "@/features/champions/components/ChampionCard"
 
 function Main() {
+  const { data: championNames } = useChampionNamesQuery()
+  const [randomizedChampionNames, setRandomizedChampionNames] = useState<
+    string[]
+  >([])
+
+  useEffect(() => {
+    let indexes: number[] = []
+    if (championNames) {
+      indexes = uniqueIndexes(championNames?.length, 12)
+      setRandomizedChampionNames(indexes.map((i) => championNames?.[i]))
+    }
+  }, [championNames])
+
+  console.log(randomizedChampionNames)
+
   return (
     <main>
-      <Container>Main</Container>
+      <Container>
+        <Scoreboard />
+        <div className="flex gap-8">
+          <ChampionCard></ChampionCard>
+          <ChampionCard></ChampionCard>
+        </div>
+      </Container>
     </main>
   )
 }
