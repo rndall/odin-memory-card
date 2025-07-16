491f5e9ffe4e049f177d54609610abf936656415
rndall <96964503+rndall@users.noreply.github.com>
2025-07-16 14:28:10 +0800

    feat: add champion card hover styles

--- src/features/champions/components/ChampionCard.tsx
+++ src/features/champions/components/ChampionCard.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card"
 
 function ChampionCard({ champion: { id, name } }: { champion: Champion }) {
   return (
-    <Card className="min-h-100 w-54">
+    <Card className="hover:bg-card/80 min-h-100 w-54 cursor-pointer">
       <CardContent className="basis-full">
         <img
           src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${id}_0.jpg`}
